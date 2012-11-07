eeb0f79e980db9b40eaebf42301db1ef1bf6f7fd
Jukka Viinamäki <jukka.viinamaki@futurice.com>
2012-11-07 09:57:10 +0200

    Added derivate case classes

--- material.scala
+++ material.scala
@@ -1,5 +1,13 @@
 package object Demo {
 
   val xs = Seq(1, 2, 3, 4, 5, 6)
+  
+  sealed trait Term
+
+  case class Number(value: Int) extends Term
+  case class Var(name: String) extends Term
+  case class Sum(a: Term, b: Term) extends Term
+  case class Prod(a: Term, b: Term) extends Term
+  case class Power(a: Term, b: Term) extends Term
 
 }
